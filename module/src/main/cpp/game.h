88ca41f8ed3191a0e64deaed71e48166e12b8865
WallD3v <109732229+WallD3v@users.noreply.github.com>
2024-04-29 23:21:04 +0600

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -3,6 +3,6 @@
 
 // TODO: change this
 #define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.axlebolt.standoff2"
+#define GamePackageName "com.skullcapstudios.bpm"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
